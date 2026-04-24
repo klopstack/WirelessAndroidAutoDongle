06a0c9756e7aa564e00966d2564013df372a9385
Ben <benklop@gmail.com>
2026-04-24 16:37:58 -0400

    fix partition creation

--- aa_wireless_dongle/board/raspberrypi/post-image.sh
+++ aa_wireless_dongle/board/raspberrypi/post-image.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-source board/raspberrypi/post-image.sh
+# Call Buildroot's Raspberry Pi post-image script directly.
+# (Don't source it: sourcing would keep $0 as this wrapper, causing it to
+# look for genimage.cfg.in in the external tree instead of Buildroot.)
+exec board/raspberrypi/post-image.sh
